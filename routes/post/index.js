715d86ef51fabfac1ff041ad6f713f3a0a086314
baaami <lgh121546@naver.com>
2022-11-06 09:59:11 +0900

    Image 연동 완료

--- routes/post/index.js
+++ routes/post/index.js
@@ -2,9 +2,24 @@ var express = require('express')
 var router = express.Router()
 var post = require('./post.ctrl')
 
+import checkToken from '../../lib/checkToken'
+
+const multer = require('multer')
+
+const storage  = multer.diskStorage({ // 2
+    destination(req, file, cb) {
+      cb(null, 'public/images/');
+    },
+    filename(req, file, cb) {
+      cb(null, `${Date.now()}__${file.originalname}`);
+    },
+});
+
+const upload = multer({ storage: storage }); // 3-2
+
 router.get('/read', post.read)
-router.post('/write', post.write)
-router.patch('/update', post.update)
+router.post('/write', upload.single('image'), post.write)
+router.patch('/update', upload.single('image'), post.update)
 router.delete('/remove', post.remove)
 
 module.exports = router
